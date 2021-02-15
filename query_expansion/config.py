478f021932a0fec8b21711e6c3c857e1e0bbc580
Dipankar Niranjan <dipankar1995@gmail.com>
2021-02-15 04:44:20 -0500

    Added config file

---
+++ query_expansion/config.py
@@ -0,0 +1,16 @@
+# For Google API
+
+search_engine_id = '14e471d67764dc04d'
+key = 'AIzaSyAENB1D2LxKbqSmJqdRSKaRVT67_XSKJ_c'
+
+# For Rocchi's algorithm
+
+ALPHA = 1
+BETA = 0.75
+GAMMA = 0.15
+
+# Region weights
+
+TITLE = 0.1
+SUMMARY = 0.7
+CONTENT = 0.2
